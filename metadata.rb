d35f19676763f512d109a4ee8d94553dea80f8bf
Alex Markessinis <markea@sage.edu>
2017-02-02 12:09:37 -0500

    initalized

--- metadata.rb
+++ metadata.rb
@@ -1,19 +1,10 @@
 name 'firefox_configuration'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'all_rights'
-description 'Installs/Configures firefox_configuration'
-long_description 'Installs/Configures firefox_configuration'
+maintainer 'Alex Markessinis'
+maintainer_email 'markea125@gmail.com'
+license 'MIT'
+description 'Configures firefox'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+issues_url 'https://github.com/MelonSmasher/chef_firefox_configuration/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/MelonSmasher/chef_firefox_configuration' if respond_to?(:source_url)
 version '0.1.0'
-
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/firefox_configuration/issues' if respond_to?(:issues_url)
-
-# The `source_url` points to the development reposiory for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/firefox_configuration' if respond_to?(:source_url)
+supports 'windows'
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,4 +2,4 @@
 # Cookbook:: firefox_configuration
 # Recipe:: default
 #
-# Copyright:: 2017, The Authors, All Rights Reserved.
+# MIT
