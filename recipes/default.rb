c7574e5c791456534f833ae53f3285c7966c9def
Alex Markessinis <markea@sage.edu>
2017-02-02 21:16:28 -0500

    fixed helpers issue.

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,7 +4,18 @@
 #
 # MIT
 
-include FirefoxConfiguration::Helpers
+def ensure_dir(path)
+  directory path do
+    action :create
+    recursive true
+  end
+end
+
+def create_template(source_template, path)
+  template path do
+    source source_template
+  end
+end
 
 # Set the firefox installation base directory
 install_path = File.join(node['firefox_configuration']['install_directory'], "")
