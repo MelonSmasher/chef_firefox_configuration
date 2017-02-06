79e03d19aea0acf0091047e1d8a80774f53b3d2b
Alex Markessinis <markea@sage.edu>
2017-02-06 17:01:45 -0500

    working on fixing escapes within js template

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Configures FireFox'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 issues_url 'https://github.com/MelonSmasher/chef_firefox_configuration/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/MelonSmasher/chef_firefox_configuration' if respond_to?(:source_url)
-version '0.1.8'
+version '0.2.0'
 supports 'windows'
 supports 'mac_os_x'
 suggest 'firefox'
